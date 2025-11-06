59e372b50574f59c9f9608e7dbb9537bb5cb18c5
olivia <oliviaenjoyslife2025@gmail.com>
2025-11-05 19:52:32 -0800

    added comment about ModelViewSet

--- taskmanager_backend/tasks/views.py
+++ taskmanager_backend/tasks/views.py
@@ -4,24 +4,16 @@ from .serializers import TaskSerializer
 
 class TaskViewSet(viewsets.ModelViewSet):
     """
-    Provides CRUD (Create, Read, Update, Delete) API endpoints for Task model.
-    
     Allowed methods:
-    - GET /v1/tasks/       (list)
-    - POST /v1/tasks/      (create)
-    - GET /v1/tasks/{id}/  (detail)
-    - PUT /v1/tasks/{id}/  (full update)
-    - PATCH /v1/tasks/{id}/ (partial update)
-    - DELETE /v1/tasks/{id}/ (delete)
+    - GET /v1/tasks/       (list - retrieve all tasks)
+    - POST /v1/tasks/      (create - create new task)
+    - GET /v1/tasks/{id}/  (retrieve - get single task)
+    - PUT /v1/tasks/{id}/  (update - full update)
+    - PATCH /v1/tasks/{id}/ (partial_update - partial update)
+    - DELETE /v1/tasks/{id}/ (destroy - delete task)
     """
     # Define queryset to get all tasks
+    # The ordering is already defined in models.py Meta class
     queryset = Task.objects.all()
     # Define serializer class to use
     serializer_class = TaskSerializer
-
-    def get_queryset(self):
-        """
-        Override this method to ensure tasks are always ordered as we want.
-        The ordering in models.py already defines sorting, this is just a redundant guarantee.
-        """
-        return Task.objects.all()
