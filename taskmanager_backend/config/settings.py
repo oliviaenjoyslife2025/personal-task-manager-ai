e0dc982021fdeb92d7ae5f194372fcf070cdbf9b
Olivia <oliviaenjoyslife2025@gmail.com>
2025-11-05 19:46:04 -0800

    Update Django part in settings.py
    Update Django part in settings.py

--- taskmanager_backend/config/settings.py
+++ taskmanager_backend/config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-od17xts(0es43#katq6k7_#qg#!@c+2hjkm$0jv#1d$((fhma("
+SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
